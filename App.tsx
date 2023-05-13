889f8ce203e41c5f7c90741c5df020ee2fe3cca4
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-05-13 20:00:45 -0300

    feat: configurado serviço de notificação One Signal

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
-import { StatusBar } from 'react-native';
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 
-import { Routes } from './src/routes';
-
-import { THEME } from './src/theme';
+import { ONE_SIGNAL_APP_ID } from './env.json';
 import { Loading } from './src/components/Loading';
-
 import { CartContextProvider } from './src/contexts/CartContext';
+import { Routes } from './src/routes';
+import { THEME } from './src/theme';
+
+OneSignal.setAppId(ONE_SIGNAL_APP_ID);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
---
+++ src/@types/env.d.ts
@@ -0,0 +1,3 @@
+declare module '@env' {
+  export const ONE_SIGNAL_APP_ID: string;
+}
