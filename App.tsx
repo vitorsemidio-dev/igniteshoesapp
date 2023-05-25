289e0c50f6a41b43e0023691a41dc8190267e9c1
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-05-24 22:48:00 -0300

    feat: redirecionamento através de notificação

--- App.tsx
+++ App.tsx
@@ -5,15 +5,11 @@ import {
 } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from 'native-base';
 import { StatusBar } from 'react-native';
-import OneSignal, {
-  NotificationReceivedEvent,
-  OSNotification,
-} from 'react-native-onesignal';
+import OneSignal from 'react-native-onesignal';
 
 import { ONE_SIGNAL_APP_ID } from '@env';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Loading } from './src/components/Loading';
-import { Notification } from './src/components/Notification';
 import { CartContextProvider } from './src/contexts/CartContext';
 import { createTagUserInfo } from './src/notifications/tagNotifications';
 import { Routes } from './src/routes';
@@ -29,26 +25,12 @@ OneSignal.promptForPushNotificationsWithUserResponse((response) => {
 });
 
 export default function App() {
-  const [notification, setNotification] = useState<OSNotification | undefined>(
-    undefined,
-  );
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   useEffect(() => {
     // createTagUserEmail(USER_EMAIL);
     // deleteTag('email');
     createTagUserInfo();
-  }, []);
-
-  useEffect(() => {
-    const unsubscribed = OneSignal.setNotificationWillShowInForegroundHandler(
-      (notificationReceivedEvent: NotificationReceivedEvent) => {
-        console.log(notificationReceivedEvent);
-        const response = notificationReceivedEvent.getNotification();
-
-        setNotification(response);
-      },
-    );
   }, []);
 
   return (
@@ -58,12 +40,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {notification?.title && (
-        <Notification
-          title={notification.title}
-          onClose={() => setNotification(undefined)}
-        />
-      )}
+
       <CartContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
       </CartContextProvider>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,44 @@
-import { useTheme } from 'native-base';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { useTheme } from 'native-base';
+import { useEffect, useState } from 'react';
+import OneSignal, {
+  NotificationReceivedEvent,
+  OSNotification,
+} from 'react-native-onesignal';
 
+import { Notification } from '../components/Notification';
 import { AppRoutes } from './app.routes';
 
 export function Routes() {
   const { colors } = useTheme();
+  const [notification, setNotification] = useState<OSNotification | undefined>(
+    undefined,
+  );
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
 
+  useEffect(() => {
+    const unsubscribed = OneSignal.setNotificationWillShowInForegroundHandler(
+      (notificationReceivedEvent: NotificationReceivedEvent) => {
+        console.log(notificationReceivedEvent);
+        const response = notificationReceivedEvent.getNotification();
+
+        setNotification(response);
+      },
+    );
+  }, []);
+
   return (
     <NavigationContainer theme={theme}>
       <AppRoutes />
+
+      {notification?.title && (
+        <Notification
+          title={notification.title}
+          onClose={() => setNotification(undefined)}
+        />
+      )}
     </NavigationContainer>
   );
 }
